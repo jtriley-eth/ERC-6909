3d2e3541ed75067f03d75a537fdfde72dea9b30e
Joshua Trujillo <jtriley15@gmail.com>
2023-04-17 11:55:12 -0700

    typos

--- src/ERCN.sol
+++ src/ERCN.sol
@@ -2,9 +2,21 @@
 pragma solidity ^0.8.19;
 
 contract ERCN {
+    /// @dev Thrown when owner balance for id is insufficient.
+    /// @param owner The address of the owner.
+    /// @param id The id of the token.
     error InsufficientBalance(address owner, uint256 id);
+
+    /// @dev Thrown when spender allowance for id is insufficient.
+    /// @param spender The address of the spender.
+    /// @param id The id of the token.
     error InsufficientPermission(address spender, uint256 id);
 
+    /// @notice The event emitted when a transfer occurs.
+    /// @param sender The address of the sender.
+    /// @param receiver The address of the receiver.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     event Transfer(
         address indexed sender,
         address indexed receiver,
@@ -12,12 +24,21 @@ contract ERCN {
         uint256 amount
     );
 
+    /// @notice The event emitted when an operator is set.
+    /// @param owner The address of the owner.
+    /// @param spender The address of the spender.
+    /// @param approved The approval status.
     event OperatorSet(
         address indexed owner,
         address indexed spender,
         bool approved
     );
 
+    /// @notice The event emitted when an approval occurs.
+    /// @param owner The address of the owner.
+    /// @param spender The address of the spender.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     event Approval(
         address indexed owner,
         address indexed spender,
@@ -25,16 +46,22 @@ contract ERCN {
         uint256 amount
     );
 
-    mapping(address owner => mapping(uint256 id => uint256 amount)) public balanceOf;
-
-    mapping(
-        address owner => mapping(address spender => mapping(uint256 id => uint256 amount))
-    ) public allowance;
-
-    mapping(address owner => mapping(address operator => bool)) public isOperator;
-
+    /// @notice The total supply of each id.
     mapping(uint256 id => uint256 amount) public totalSupply;
 
+    /// @notice Owner balance of an id.
+    mapping(address owner => mapping(uint256 id => uint256 amount)) public balanceOf;
+
+    /// @notice Spender allowance of an id.
+    mapping(address owner => mapping(address spender => mapping(uint256 id => uint256 amount))) public allowance;
+
+    /// @notice Checks if a spender is approved by an owner as an operator
+    mapping(address owner => mapping(address spender => bool)) public isOperator;
+
+    /// @notice Transfers an amount of an id from the caller to a receiver.
+    /// @param receiver The address of the receiver.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     function transfer(address receiver, uint256 id, uint256 amount) public {
         if (balanceOf[msg.sender][id] < amount) revert InsufficientBalance(msg.sender, id);
         balanceOf[msg.sender][id] -= amount;
@@ -42,6 +69,11 @@ contract ERCN {
         emit Transfer(msg.sender, receiver, id, amount);
     }
 
+    /// @notice Transfers an amount of an id from a sender to a receiver.
+    /// @param sender The address of the sender.
+    /// @param receiver The address of the receiver.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     function transferFrom(address sender, address receiver, uint256 id, uint256 amount) public {
         if (sender != msg.sender && !isOperator[sender][msg.sender]) {
             if (allowance[sender][msg.sender][id] < amount)
@@ -54,11 +86,18 @@ contract ERCN {
         emit Transfer(sender, receiver, id, amount);
     }
 
+    /// @notice Approves an amount of an id to a spender.
+    /// @param spender The address of the spender.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     function approve(address spender, uint256 id, uint256 amount) public {
         allowance[msg.sender][spender][id] = amount;
         emit Approval(msg.sender, spender, id, amount);
     }
 
+    /// @notice Sets or unsets a spender as an operator for the caller.
+    /// @param spender The address of the spender.
+    /// @param approved The approval status.
     function setOperator(address spender, bool approved) public {
         isOperator[msg.sender][spender] = approved;
         emit OperatorSet(msg.sender, spender, approved);
--- src/IERCN.sol
+++ src/IERCN.sol
@@ -2,19 +2,33 @@
 pragma solidity ^0.8.19;
 
 interface IERCN {
+    /// @notice The event emitted when a transfer occurs.
+    /// @param sender The address of the sender.
+    /// @param receiver The address of the receiver.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     event Transfer(
-        address indexed from,
-        address indexed to,
+        address indexed sender,
+        address indexed receiver,
         uint256 indexed id,
         uint256 amount
     );
 
+    /// @notice The event emitted when an operator is set.
+    /// @param owner The address of the owner.
+    /// @param spender The address of the spender.
+    /// @param approved The approval status.
     event OperatorSet(
         address indexed owner,
-        address indexed operator,
+        address indexed spender,
         bool approved
     );
 
+    /// @notice The event emitted when an approval occurs.
+    /// @param owner The address of the owner.
+    /// @param spender The address of the spender.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     event Approval(
         address indexed owner,
         address indexed spender,
@@ -22,21 +36,55 @@ interface IERCN {
         uint256 amount
     );
 
+    /// @notice The total supply of each id.
+    /// @param id The id of the token.
+    /// @return amount The total supply of the token.
     function totalSupply(uint256 id) external view returns (uint256 amount);
 
+    /// @notice Owner balance of an id.
+    /// @param owner The address of the owner.
+    /// @param id The id of the token.
+    /// @return amount The balance of the token.
     function balanceOf(address owner, uint256 id) external view returns (uint256 amount);
 
+    /// @notice Spender allowance of an id.
+    /// @param owner The address of the owner.
+    /// @param spender The address of the spender.
+    /// @param id The id of the token.
+    /// @return amount The allowance of the token.
     function allowance(
         address owner,
         address spender,
         uint256 id
     ) external view returns (uint256 amount);
 
+    /// @notice Checks if a spender is approved by an owner as an operator
+    /// @param owner The address of the owner.
+    /// @param spender The address of the spender.
+    /// @return approved The approval status.
+    function isOperator(address owner, address spender) external view returns (bool approved);
+
+    /// @notice Transfers an amount of an id from the caller to a receiver.
+    /// @param receiver The address of the receiver.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     function transfer(address receiver, uint256 id, uint256 amount) external;
 
+    /// @notice Transfers an amount of an id from a sender to a receiver.
+    /// @param sender The address of the sender.
+    /// @param receiver The address of the receiver.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     function transferFrom(address sender, address receiver, uint256 id, uint256 amount) external;
 
+    /// @notice Approves an amount of an id to a spender.
+    /// @param spender The address of the spender.
+    /// @param id The id of the token.
+    /// @param amount The amount of the token.
     function approve(address spender, uint256 id, uint256 amount) external;
 
-    function setOperator(address operator, bool approved) external;
+    /// @notice Sets or unsets a spender as an operator for the caller.
+    /// @param spender The address of the spender.
+    /// @param approved The approval status.
+    function setOperator(address spender, bool approved) external;
 }
